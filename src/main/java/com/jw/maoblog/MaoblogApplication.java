70982dd7c4b250b94617d5f6030a3fc8b90c6514
jsonw99 <ningweiw@gmail.com>
2017-12-31 18:53:13 -0800

    config user functions

---
+++ src/main/java/com/jw/maoblog/MaoblogApplication.java
@@ -0,0 +1,12 @@
+package com.jw.maoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaoblogApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jw/maoblog/domain/User.java
@@ -0,0 +1,65 @@
+package com.jw.maoblog.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+public class User implements UserDetails, Serializable{
+    private static final long serialVersionId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "name cannot be empty.")
+    @Size(max=50)
+
+
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
